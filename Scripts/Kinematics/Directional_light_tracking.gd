f8a1b744a6baaded41841abe28282ab7a68ff0e5
Roman Yeremenko <roal.yr@gmail.com>
2023-01-30 13:09:47 +0200

    IMPROVEMENT: Star chart reworked.

---
+++ Scripts/Kinematics/Directional_light_tracking.gd
@@ -0,0 +1,14 @@
+extends DirectionalLight
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	self.look_at(Paths.player.transform.origin, Vector3(1,0,0))
