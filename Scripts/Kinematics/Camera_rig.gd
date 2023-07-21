eaec4b8dc71ca9996bd8ad271283eb310f16df0b
Roman Yeremenko <roal.yr@gmail.com>
2023-07-21 15:16:45 +0300

    IMPROVEMENT, REFACTOR: Global nebula, redundant models

--- Scripts/Kinematics/Camera_rig.gd
+++ Scripts/Kinematics/Camera_rig.gd
@@ -22,7 +22,7 @@ const camera_fov_derivative = 2
 const camera_fov_max_delta = 140 - Constants.camera_fov
 const camera_brightness_velocity_factor = 2e-4
 const camera_brightness_derivative = 0.1
-const camera_brightness_max_delta = 2.0
+const camera_brightness_max_delta = 1.5
 # TODO: adjust background colors separatenly?
 
 # Values.
