feab774b941015d5dec2885f74da18a84eb38d19
Roman Yeremenko <roal.yr@gmail.com>
2026-02-26 00:35:51 +0200

    Art works (nebula, materials, starspheres etc)

---
+++ src/scenes/game_world/starsphere_slot.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+
+var main_camera : Camera = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.global_position = GlobalRefs.main_camera.global_position
